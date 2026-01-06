9b9f9d3fa0bc6bfd378c246366b9f70bd3d53e33
Talesoftoba <Tobaking4@gmail.com>
2026-01-06 17:45:17 +0100

    fix 7

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import Login from "./Pages/Login/Login";
 
 function App() {
 
+    function setVh(){
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    window.addEventListener(`resize`,setVh);
+    setVh();
+
     return(
     <Login/>
     );
