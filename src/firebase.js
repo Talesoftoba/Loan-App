1093c2427d60a998e00e357a831837f2fea97d72
Talesoftoba <Tobaking4@gmail.com>
2026-01-07 18:22:04 +0100

    config debug

--- src/firebase.js
+++ src/firebase.js
@@ -9,8 +9,13 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+console.log("Firebase Config:",{
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  projectId:import.meta.VITE_FIREBASE_PROJECT_ID,
+})
 
 const app = initializeApp(firebaseConfig);
 
