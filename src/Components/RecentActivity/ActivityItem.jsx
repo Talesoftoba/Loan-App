db2a67c8631ea08009bad332e3790fc9688835a9
Talesoftoba <Tobaking4@gmail.com>
2026-01-09 02:09:09 +0100

    updated recent activity

--- src/Components/RecentActivity/ActivityItem.jsx
+++ src/Components/RecentActivity/ActivityItem.jsx
@@ -3,22 +3,18 @@ import styles from "./RecentActivity.module.css";
 const ActivityItem = ({ title, time, icon: Icon, color, isLast }) => {
   return (
     <div className={styles.item}>
-      {/* Timeline */}
-      <div className={styles.track}>
-        <span className={`${styles.dot} ${styles[color]}`} />
-        {!isLast && <span className={styles.line} />}
+      {/* Timeline column */}
+      <div className={styles.timelineCol}>
+        <span className={styles.line} />
+        <div className={`${styles.icon} ${styles[color]}`}>
+          <Icon size={16} />
+        </div>
       </div>
 
       {/* Content */}
       <div className={styles.content}>
-        <div className={`${styles.icon} ${styles[color]}`}>
-          <Icon size={16} />
-        </div>
-
-        <div>
-          <p className={styles.title}>{title}</p>
-          <span className={styles.time}>{time}</span>
-        </div>
+        <p className={styles.title}>{title}</p>
+        <span className={styles.time}>{time}</span>
       </div>
     </div>
   );
